92f146a7e036c1f4c22911a2fa70a93bfafb4729
Caleb Johnstone <calebdjohnstone@gmail.com>
2020-08-03 15:40:47 +0200

    Add files via upload

---
+++ WTW285/5.6/Rabbits/Main.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.math.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args){
+        //Epp Exercise Set 5.6 (22 and 23)
+        int initialPairs = Integer.parseInt(args[0]);
+        int monthsToBeFertile = Integer.parseInt(args[1]);
+        int n = Integer.parseInt(args[2]);
+
+        int numberOfTerms = Integer.parseInt(args[3]);
+
+        Queue<Integer> r = new LinkedList<Integer>();
+
+        //only one pair of rabbits while they are not fertile yet
+        for(int i = 0; i <= monthsToBeFertile; i++){
+            r.add(initialPairs);
+            System.out.println("r(" + i + "): " + initialPairs);
+        }
+
+        int previousTerm = 1;
+
+        //compute the terms after this
+        for(int i = monthsToBeFertile+1; i <= numberOfTerms; i++){
+            int currentTerm = previousTerm + offSpringRate*(r.remove());
+
+            r.add(currentTerm);
+            System.out.println("r(" + i + "): " + currentTerm);
+
+            previousTerm = currentTerm;
+        }
+    }
+}
